931f68b485c2a457b2b02a0a3e6f249fad186ca5
KR1023 <shyoo1023@gmail.com>
2021-07-25 17:44:22 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './js/main.js',
+  output: {
+    clean: true
+  },
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [{
+        from: 'static'
+      }]
+    })
+  ],
+  module: {
+    rules: [{
+      test: /.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'postcss-loader',
+        'sass-loader'
+      ]
+    },
+    {
+      test: /\.js$/,
+      use: [
+        'babel-loader'
+      ]
+    }
+  ]
+  }
+}
